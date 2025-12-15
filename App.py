3e9ddef07182e842396d1f2377705b9dafa4558f
rui-david <ruidavid28@gmail.com>
2025-12-15 11:16:31 -0100

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 
 st.header("Introduzindo elementos Streamlit")
 menu = option_menu(menu_title="Menu", 
-                   options=["Inicio", "Gráficos Estáticos", "Gráficos Dinâmicos", "widgets", "Formulário"]
+                   options=["Inicio", "Gráficos Estáticos", "Gráficos Dinâmicos", "widgets", "Formulário"],
                    icons=["house", "bar-chart-line", "toggles", "bar-chart"],
                    menu_icon="cast",
                    default_index=0,
